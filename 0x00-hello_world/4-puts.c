bdc58be222f28876d26ef0901d2310df64d04545
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-14 02:04:23 +0100

    Print into the standard output using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* Description: print into the oupt using puts function
+*/
+
+int	main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
