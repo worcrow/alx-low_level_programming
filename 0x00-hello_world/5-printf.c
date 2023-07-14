639a2c2ac232c797ab1a4a55d13bcfaa67ee49b9
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-14 02:22:09 +0100

    Print to the standard output using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - output to the standard output
+ * Description:this function print to the standard output using printf function
+ * Return: this function return 0, to stop the execution of the programme
+ */
+
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
