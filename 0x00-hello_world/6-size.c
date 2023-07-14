59dd4e19551e575da590c0f33cb6fb8e4c890084
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-14 02:42:13 +0100

    prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - output the size of types 
+ * Description: print the various size type to the standard output
+ * Return: this function return 0, to stop the execution of the programme
+ */
+
+int	main(void)
+{
+	printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(char));
+	printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(float));
+	return (0);
+}
