e5e99a2cc1d82babeedcd3a861c760e21745d109
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-15 23:08:38 +0100

    modify 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Description: print a message to the standard error
+ * Return: 1 , encounter an error
+*/
+
 int	main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return -1;
+	char *message;
+
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fprintf(stderr, "%s", message);
+	return (-1);
 }
