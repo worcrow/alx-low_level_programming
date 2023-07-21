0b1c446105c62ca34ddc6322dea8d530025c8844
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-21 03:57:09 +0100

    update the file 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -20,15 +20,3 @@ void print_most_numbers(void)
 	_putchar('\n');
 }
 
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_most_numbers();
-    return (0);
-}
