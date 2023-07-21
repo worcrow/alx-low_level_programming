ad7800148c8bc73fcc770dce8090e4e6bbce2c00
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-21 03:31:00 +0100

    Add the file 4-print_most_numbers and Update the file main.h

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print the number from 0 to 9 except 2 and 4
+*/
+
+void print_most_numbers(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 9)
+	{
+		if (c != 2 && c != 4)
+			_putchar(c + '0');
+		c++;
+	{
+}
