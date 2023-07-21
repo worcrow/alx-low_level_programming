dc3810ac5f742a252c2693f2e413f525b5ebae1d
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-21 03:52:35 +0100

    Add the file 5-more_numbers.c and Update main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *print_number - output the num to the standard output
+ *@num: the number to be printed
+*/
+
+void print_number(int num)
+{
+	if (num < 10)
+		_putchar(num + '0');
+	else
+	{
+		print_10_number(num / 10);
+		_putchar((num % 10) + '0');
+	}
+}
+/**
+ *more_numbers - print the numbers from 0 to 14 10 times
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 15 ; j++)
+		{
+			print_number(j);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
