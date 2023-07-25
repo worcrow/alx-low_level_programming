531cb3d66ff78a93c0b87def65a8efcaa3e1b49e
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-25 02:30:11 +0100

    Update the 6-puts2.c file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,23 @@
 #include "main.h"
+
+/**
+ *length - return the length of the string
+ *@str: string variable
+ *Return: return the length
+*/
+
+int length(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
 /**
  *puts2 - print every other string starting from the first
  *@str: the string variable
@@ -7,9 +26,11 @@
 void puts2(char *str)
 {
 	int i;
+	int len;
 
 	i = 0;
-	while (str[i] != '\0')
+	len = length(str);
+	while (i < len)
 	{
 		_putchar(str[i]);
 		i += 2;
