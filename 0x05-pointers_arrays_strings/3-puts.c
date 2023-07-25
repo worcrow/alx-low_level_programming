d39a463b2f799754ccf3ee3b109e15284336d00e
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-25 01:45:18 +0100

    prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts - output the string to the stdout
+ *@str: the string to be printed
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
