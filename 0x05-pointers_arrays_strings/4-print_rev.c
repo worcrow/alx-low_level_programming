5a7f628dc42de19501176ac66dbf75920bd1725d
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-25 01:55:41 +0100

    prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *length - return the length of the string
+ *@s: the string variable
+ *Return: the lenght of the string
+*/
+
+int length(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ *print_rev - output the string to the stdout in reverse order
+ *@s: the string variable
+*/
+
+void print_rev(char *s)
+{
+	int len;
+
+	len = length(s) - 1;
+	while (len > 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
