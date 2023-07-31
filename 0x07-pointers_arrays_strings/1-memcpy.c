b76af98ab9a459d3dc0697d7788d7d8ffd9cd658
worcrow <58397070+worcrow@users.noreply.github.com>
2023-07-31 08:21:15 +0100

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_memcpy - copy n bytes from src to dest
+ *@src: source of string to be copied
+ *@dest: destination to be be filled in
+ *@n: the size to be copied
+ *Return: return a pointer to the resulting string
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		*dest = src[i++];
+		dest++;
+	}
+	return (dest - n);
+}
