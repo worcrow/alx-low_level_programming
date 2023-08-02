d70ac968d4a2d8da5db0b008b8582e01cae53708
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-02 18:51:40 +0100

    prints a string in reverse using recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - print string in reverse order using recursion
+ *@s: string to be printed
+*/
+
+void _print_rev_recursion(char *s)
+{
+	char tem; /* temporary variable to strore the first chararctere of s */
+
+	if (*s != '\0')
+	{
+		temp = *s;
+		_print_rev_recursion(++s);
+		_putchar(temp);
+	}
+}
+
