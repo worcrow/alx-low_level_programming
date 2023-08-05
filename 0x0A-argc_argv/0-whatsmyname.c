2fc299eba9c73e365c0658d0ed17de939f42873a
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-05 19:47:54 +0100

    modify main.h and 0-whatsmyname files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *main - print the name of the program
  *@argc: argument count
@@ -7,6 +8,7 @@
 */
 int main(int argc, char *argv[])
 {
-	_puts(argv[argc - 1]);
+	if (argc > 0)
+		printf("%s", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
 	void _puts(char *s);
-	int _putchar(char c);
 #endif
