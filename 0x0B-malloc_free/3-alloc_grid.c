78b2121c73e185f87d53fd9f15cb868518e040cc
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-10 03:08:49 +0100

    update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,10 +21,11 @@ int **alloc_grid(int width, int height)
 			array_2D[i] = (int *) malloc(width * sizeof(int));
 			if (array_2D[i] == NULL)
 			{
-				for (i ; i >= 0 ; i--)
+				for (i-- ; i >= 0 ; i--)
 				{
 					free(array_2D[i]);
 				}
+				free(grid);
 				return (NULL);
 			}
 		}
