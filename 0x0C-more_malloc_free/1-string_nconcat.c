2723b5e4f48803a31125b9b99c3b3b7dc471ee46
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-12 04:24:02 +0100

    update 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,63 +1,46 @@
 #include <stdlib.h>
 
 /**
- * length_of - calculate the length of a string
- * @s: string to be calculated it length
- * Return: return the length of s
-*/
-
-unsigned int length_of(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * string_nconcat - concatenate two string
+ * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: the byte to be copied from s2
+ * @n: n byte to be copied from s2
+ * Return: pointer to s1 plus n byte of s2
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int index; /* track the index of the arr */
-	unsigned int start; /* track the index to be copied from of s2 */
-	char *arr; /* array that hold the concatenation of the string s1 and s2 */
-	unsigned int size; /* size of the array 'arr' */
+	unsigned int i, j, len1, len2;
+	char *arr;
 
-	size = length_of(s1) + n + 1;
-	index = 0;
-	arr = malloc(size * sizeof(char));
-	if (arr != NULL)
+	len1 = 0;
+	len2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1] != '\0')
 	{
-		if (s1 != NULL)
-		{
-			while (*s1 != '\0')
-			{
-				arr[index] = *s1;
-				s1++;
-				index++;
-			}
-		}
-		if (s2 != NULL)
-		{
-			for (start = 0; s2[start] != '\0' && start < n; start++)
-			{
-				arr[index++] = s2[start];
-			}
-		}
-		while (index < size)
-		{
-			arr[index++] = '\0';
-		}
+		len1++;
 	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	if (n > len2)
+		n = len2;
+	arr = malloc(len1 + n + 1);
+	if (!arr)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+	{
+		arr[i] = s1[i];
+	}
+	for (j = 0; j < n; j++, i++)
+	{
+		arr[i] = s2[j];
+	}
+	arr[i] = '\0';
 	return (arr);
 }
 
