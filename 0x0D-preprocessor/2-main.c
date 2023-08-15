a6a54e7febd9ee3ab02dcb3c5e9935be014e6ea1
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-15 18:07:52 +0100

    update 2-main.c file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,21 +1,4 @@
-/**
- * print_string - print the string given as argument to the stdout
- * @str: string to be printed
-*/
-
-void print_string(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
-}
-
+#include <stdio.h>
 /**
  * main - print the name of the current file
  * Return: always 0
@@ -23,8 +6,6 @@ void print_string(char *str)
 
 int main(void)
 {
-	char name_file[] = __FILE__;
-
-	print_string(name_file);
+	printf("%s\n", __FILE__);
 	return (0);
 }
