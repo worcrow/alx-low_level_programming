c489ba51ae1cde385cc701422cbf1ea8ad325eec
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-16 00:53:02 +0100

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+/**
+ * struct dog - structure contains informations about the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+	struct dog
+	{
+		char *name;
+		int age;
+		char *owner;
+	}
+#endif
