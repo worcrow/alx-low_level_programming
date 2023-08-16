111f605c41aad4e0df849aa9f5187b461816c41c
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-16 02:45:58 +0100

    update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	/*A pointer has already been passed, we do not need to create again*/
 	if (d == NULL)
 		return;
-
+	if (name == NULL)
+		return;
+	if (owner == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
