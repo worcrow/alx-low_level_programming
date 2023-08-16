a7ed3247cfa29ba6a2ac2a33cd5613d419202be3
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-16 23:38:46 +0100

    update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef HEADER_H
 #define HEADER_H
+#define NULL ((void *)0)
+#define size_t unsigned int
 	int _putchar(char c);
 	void print_name(char *name, void (*f)(char *));
 	void array_iterator(int *array, size_t size, void (*action)(int));
