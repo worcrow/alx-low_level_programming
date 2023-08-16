6474ec2eea3fae3a45583dd58c983e7e45ff7fa1
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-16 22:04:18 +0100

    prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name given as parameters
+ * @name: pointer to the string name
+ * @f: function pointer to print name
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
