1e3415a71cc49ab06a6aece9dba4f49b6d662a01
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-16 22:25:54 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name
- * @name: The name to prints
- * @f: Pointer to function
+ * print_name - Prints a the name given as argument
+ * @name: pointer string to the name to be printed
+ * @f: Pointer to function that print name
  *
  * Return: Nothing
 */
