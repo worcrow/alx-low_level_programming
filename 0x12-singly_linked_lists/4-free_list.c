3b9396346679b25741a3e0cbf89c42d759db6e68
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-25 00:35:59 +0100

    updae 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 
 /**
+ * free_list - free the linked list
+ * @head: the head of the linked list
 */
 
 void free_list(list_t *head)
 {
 	if (head == NULL)
-		return ;
+		return;
 	free_list(head->next);
 	free(head->str);
 	free(head);
