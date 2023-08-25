d5f5a538272a45651fd2011a1279a022919abddc
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-25 01:28:03 +0100

    update lists.h, add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_before_main - print a string before execution of main function
+*/
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,4 +22,5 @@
 	list_t *add_node(list_t **head, const char *str);
 	list_t *add_node_end(list_t **head, const char *str);
 	void free_list(list_t *head);
+	void print_before_main() __attribute__((constructor));
 #endif
