f0db0e4aa16c5a2b699be305d241a6794c8aa3da
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-25 01:28:42 +0100

    update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,5 +22,5 @@
 	list_t *add_node(list_t **head, const char *str);
 	list_t *add_node_end(list_t **head, const char *str);
 	void free_list(list_t *head);
-	void print_before_main() __attribute__((constructor));
+	void print_before_main(void) __attribute__((constructor));
 #endif
