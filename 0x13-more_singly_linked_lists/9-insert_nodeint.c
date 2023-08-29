02abd34240323aefd7e799a6586ba1bb80f4182b
worcrow <58397070+worcrow@users.noreply.github.com>
2023-08-29 19:28:11 +0100

    update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,32 +16,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count;
 
 	temp = *head;
-	count = -1;
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL || (temp == NULL && idx != 0))
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-	if (temp == NULL && idx == 0)
-		*head = new_node;
-	else if (idx == 0)
+	if (idx != 0)
 	{
-		new_node->next = temp;
-		*head = new_node;
-		count++;
-	}
-	else
-	{
-		while (temp)
+		for (count = 0; count < idx - 1 && temp; count++)
 		{
-			if ((++count) + 1 == idx)
-			{
-				new_node->next = temp->next;
-				temp->next = new_node;
-				break;
-			}
 			temp = temp->next;
 		}
 	}
-	return (count > idx ? NULL : new_node);
+	if (temp == NULL && idx != 0)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = temp->next;
+		temp->next = new_node;
+	}
+	return (new_node);
 }
