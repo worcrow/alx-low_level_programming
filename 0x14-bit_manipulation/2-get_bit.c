9f729fbe63842d9e15939d3ffe5e6b5287448bab
worcrow <58397070+worcrow@users.noreply.github.com>
2023-09-01 20:37:21 +0100

    update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * convert_to_binary - convert a number to base2
- * @n: number to be converted
- * @arr: arr to store the value of the converted variable
- * Return: the lenght of the array
-*/
-
-unsigned int convert_to_binary(unsigned long int n, int *arr)
-{
-	int len;
-
-	len = 0;
-	while (n > 0)
-	{
-		arr[len++] = (n & 1);
-		n = n >> 1;
-	}
-	return (len);
-}
-
-/**
  * get_bit - get the value of a bit at a given index
  * @n: the number to be extracted from
  * @index: index of the target bit
@@ -29,11 +9,7 @@ unsigned int convert_to_binary(unsigned long int n, int *arr)
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int arr[BUFF_SIZE];
-	unsigned int len;
-
-	len = convert_to_binary(n, arr);
-	if (index >= len && index > 63)
+	if (index > 63)
 		return (-1);
-	return (arr[index]);
+	return ((n >> index) & 1);
 }
