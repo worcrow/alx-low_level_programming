3db8de97a32cbb1b53e8004a3e216b3accba272e
worcrow <58397070+worcrow@users.noreply.github.com>
2023-09-01 22:54:29 +0100

    update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,14 +10,14 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int flip;
+	unsigned long int flip;
 	unsigned int count;
 
 	count = 0;
 	flip = n ^ m;
 	while (flip)
 	{
-		if ((flip & 1) == 1)
+		if (flip & 1)
 			count++;
 		flip >>= 1;
 	}
