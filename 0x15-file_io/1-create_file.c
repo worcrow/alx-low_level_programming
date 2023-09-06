d8438f218c40b77db10d3e1788772eeaa18cfd0a
worcrow <58397070+worcrow@users.noreply.github.com>
2023-09-06 22:58:48 +0100

    update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 /**
+ * create_file - creates a file
+ * @filename: the name of the file to be created
+ * @text_content: content to be write into the file
+ * Return: 1 if the function succeed, otherwise -1
 */
 
 int create_file(const char *filename, char *text_content)
